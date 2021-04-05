d733fcc05ad52d3753926aa9a21bb2d506c7c652
kazimirm <kazimir.majo@gmail.com>
2021-04-05 17:58:05 +0200

    Initial commit

---
+++ src/main/java/Objects/Poskytovatel.java
@@ -0,0 +1,301 @@
+package Objects;
+
+public class Poskytovatel {
+    String mistoPoskytovaniId;
+    String zdravotnickeZarizeniId;
+    String kod;
+    String nazevZarizeni;
+    String druhZarizeni;
+    String obec;
+    String psc;
+    String ulice;
+    String cisloDomovniOrientacni;
+    String kraj;
+    String krajCode;
+    String okres;
+    String okresCode;
+    String spravniObvod;
+    String poskytovatelTelefon;
+    String poskytovatelFax;
+    String poskytovatelEmail;
+    String poskytovatelWeb;
+    String ico;
+    String typOsoby;
+    String pravniFormaKod;
+    String krajCodeSidlo;
+    String okresCodeSidlo;
+    String obecSidlo;
+    String pscSidlo;
+    String uliceSidlo;
+    String cisloDomovniOrientacniSidlo;
+    String oborPece;
+    String formaPece;
+    String druhPece;
+    String odbornyZastupce;
+    String lat;
+    String lng;
+
+    public String getMistoPoskytovaniId() {
+        return mistoPoskytovaniId;
+    }
+
+    public void setMistoPoskytovaniId(String mistoPoskytovaniId) {
+        this.mistoPoskytovaniId = mistoPoskytovaniId;
+    }
+
+    public String getZdravotnickeZarizeniId() {
+        return zdravotnickeZarizeniId;
+    }
+
+    public void setZdravotnickeZarizeniId(String zdravotnickeZarizeniId) {
+        this.zdravotnickeZarizeniId = zdravotnickeZarizeniId;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
+    public String getNazevZarizeni() {
+        return nazevZarizeni;
+    }
+
+    public void setNazevZarizeni(String nazevZarizeni) {
+        this.nazevZarizeni = nazevZarizeni;
+    }
+
+    public String getDruhZarizeni() {
+        return druhZarizeni;
+    }
+
+    public void setDruhZarizeni(String druhZarizeni) {
+        this.druhZarizeni = druhZarizeni;
+    }
+
+    public String getObec() {
+        return obec;
+    }
+
+    public void setObec(String obec) {
+        this.obec = obec;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getCisloDomovniOrientacni() {
+        return cisloDomovniOrientacni;
+    }
+
+    public void setCisloDomovniOrientacni(String cisloDomovniOrientacni) {
+        this.cisloDomovniOrientacni = cisloDomovniOrientacni;
+    }
+
+    public String getKraj() {
+        return kraj;
+    }
+
+    public void setKraj(String kraj) {
+        this.kraj = kraj;
+    }
+
+    public String getKrajCode() {
+        return krajCode;
+    }
+
+    public void setKrajCode(String krajCode) {
+        this.krajCode = krajCode;
+    }
+
+    public String getOkres() {
+        return okres;
+    }
+
+    public void setOkres(String okres) {
+        this.okres = okres;
+    }
+
+    public String getOkresCode() {
+        return okresCode;
+    }
+
+    public void setOkresCode(String okresCode) {
+        this.okresCode = okresCode;
+    }
+
+    public String getSpravniObvod() {
+        return spravniObvod;
+    }
+
+    public void setSpravniObvod(String spravniObvod) {
+        this.spravniObvod = spravniObvod;
+    }
+
+    public String getPoskytovatelTelefon() {
+        return poskytovatelTelefon;
+    }
+
+    public void setPoskytovatelTelefon(String poskytovatelTelefon) {
+        this.poskytovatelTelefon = poskytovatelTelefon;
+    }
+
+    public String getPoskytovatelFax() {
+        return poskytovatelFax;
+    }
+
+    public void setPoskytovatelFax(String poskytovatelFax) {
+        this.poskytovatelFax = poskytovatelFax;
+    }
+
+    public String getPoskytovatelEmail() {
+        return poskytovatelEmail;
+    }
+
+    public void setPoskytovatelEmail(String poskytovatelEmail) {
+        this.poskytovatelEmail = poskytovatelEmail;
+    }
+
+    public String getPoskytovatelWeb() {
+        return poskytovatelWeb;
+    }
+
+    public void setPoskytovatelWeb(String poskytovatelWeb) {
+        this.poskytovatelWeb = poskytovatelWeb;
+    }
+
+    public String getIco() {
+        return ico;
+    }
+
+    public void setIco(String ico) {
+        this.ico = ico;
+    }
+
+    public String getTypOsoby() {
+        return typOsoby;
+    }
+
+    public void setTypOsoby(String typOsoby) {
+        this.typOsoby = typOsoby;
+    }
+
+    public String getPravniFormaKod() {
+        return pravniFormaKod;
+    }
+
+    public void setPravniFormaKod(String pravniFormaKod) {
+        this.pravniFormaKod = pravniFormaKod;
+    }
+
+    public String getKrajCodeSidlo() {
+        return krajCodeSidlo;
+    }
+
+    public void setKrajCodeSidlo(String krajCodeSidlo) {
+        this.krajCodeSidlo = krajCodeSidlo;
+    }
+
+    public String getOkresCodeSidlo() {
+        return okresCodeSidlo;
+    }
+
+    public void setOkresCodeSidlo(String okresCodeSidlo) {
+        this.okresCodeSidlo = okresCodeSidlo;
+    }
+
+    public String getObecSidlo() {
+        return obecSidlo;
+    }
+
+    public void setObecSidlo(String obecSidlo) {
+        this.obecSidlo = obecSidlo;
+    }
+
+    public String getPscSidlo() {
+        return pscSidlo;
+    }
+
+    public void setPscSidlo(String pscSidlo) {
+        this.pscSidlo = pscSidlo;
+    }
+
+    public String getUliceSidlo() {
+        return uliceSidlo;
+    }
+
+    public void setUliceSidlo(String uliceSidlo) {
+        this.uliceSidlo = uliceSidlo;
+    }
+
+    public String getCisloDomovniOrientacniSidlo() {
+        return cisloDomovniOrientacniSidlo;
+    }
+
+    public void setCisloDomovniOrientacniSidlo(String cisloDomovniOrientacniSidlo) {
+        this.cisloDomovniOrientacniSidlo = cisloDomovniOrientacniSidlo;
+    }
+
+    public String getOborPece() {
+        return oborPece;
+    }
+
+    public void setOborPece(String oborPece) {
+        this.oborPece = oborPece;
+    }
+
+    public String getFormaPece() {
+        return formaPece;
+    }
+
+    public void setFormaPece(String formaPece) {
+        this.formaPece = formaPece;
+    }
+
+    public String getDruhPece() {
+        return druhPece;
+    }
+
+    public void setDruhPece(String druhPece) {
+        this.druhPece = druhPece;
+    }
+
+    public String getOdbornyZastupce() {
+        return odbornyZastupce;
+    }
+
+    public void setOdbornyZastupce(String odbornyZastupce) {
+        this.odbornyZastupce = odbornyZastupce;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
